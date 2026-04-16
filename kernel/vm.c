d9bfc91f7854ab542da0a0f02bd61bf6766c2a10
chovy02 <luonggiabao0202@gmail.com>
2026-04-16 17:43:21 +0700

    4.1.2

--- kernel/vm.c
+++ kernel/vm.c
@@ -488,17 +488,38 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 
 #ifdef LAB_PGTBL
-void
-vmprint(pagetable_t pagetable) {
-  // your code here
-}
-#endif
-
-
-
-#ifdef LAB_PGTBL
 pte_t*
 pgpte(pagetable_t pagetable, uint64 va) {
   return walk(pagetable, va, 0);
+}
+#endif
+
+void
+vmprint_walk(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V){
+      if (level == 1) printf("..");
+      else if (level == 2) printf(".. ..");
+      else if (level == 3) printf(".. .. ..");
+
+      uint64 pa = PTE2PA(pte);
+
+      printf("%d: pte %p pa %p \n", i, (void*)pte, (void*)pa);
+
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        vmprint_walk((pagetable_t)pa, level + 1);
+      }
+    }
+  }
+}
+
+#ifdef LAB_PGTBL
+void
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", (void*)pagetable);
+  vmprint_walk(pagetable, 1);
 }
 #endif
